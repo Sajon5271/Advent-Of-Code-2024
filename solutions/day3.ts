1ed1411e2b7d32a667d81432845c09c8e1c9622f
Sirajis Salekin Sajon <sajon5271@gmail.com>
2024-12-03 22:56:43 +0600

    Done with day 3 part 2

--- solutions/day3.ts
+++ solutions/day3.ts
@@ -3,10 +3,20 @@ import { readTestFileContentForDay } from '../utils/index';
 const fileContent = readTestFileContentForDay(3);
 
 const allMatchingMultiplyInstructions = [
-  ...fileContent.matchAll(/mul\(\d{1,3},\d{1,3}\)/g),
+  ...fileContent.matchAll(/mul\(\d{1,3},\d{1,3}\)|don\'t\(\)|do\(\)/g),
 ].map((out) => out[0]);
 
+let currentlyOn = true;
 const total = allMatchingMultiplyInstructions.reduce((acc, curr) => {
+  if (curr === "don't()") {
+    currentlyOn = false;
+    return acc;
+  }
+  if (curr === 'do()') {
+    currentlyOn = true;
+    return acc;
+  }
+  if (!currentlyOn) return acc;
   const multiplied = curr
     .slice(4, -1)
     .split(',')
